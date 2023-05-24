3333d7873bf5dc4061c33a5a4ccb5577fab3ab31
Harsheeta <harsheeta.kap@gmail.com>
2023-05-24 16:35:03 +0530

    first server response

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const PORT = 8081
 const app = express()
 
 app.get('/',(req,res)=>{
-    console.log(req.headers)
-    console.log(req.path)
-
-    console.log('Hello World')
+    res.status(200).send('First response from express  !!')
 })
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
